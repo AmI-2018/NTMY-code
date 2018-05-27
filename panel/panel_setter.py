ceff058d56a96e69e1e9a383f95d9e3001b4f6c5
Raffaele Tuzzo <37403051+Raffot27@users.noreply.github.com>
2018-05-27 17:53:07 +0200

    Update panel_setter.py

--- panel/panel_setter.py
+++ panel/panel_setter.py
@@ -25,10 +25,12 @@ rgb_led[7]=[]
 
 def rgb_setter (num_led, red_val, green_val, blue_val):
     {
-        red_led = PWMLED(rgb_led[num_led][0])
+        #association for software pwm modulation  
+        red_led = PWMLED(rgb_led[num_led][0])   
         green_led = PWMLED(rgb_led[num_led] [1])
         blue_led = PWMLED(rgb_led[num_led] [2])
-
+        
+        #set for each pin of the led
         red_led.value=red_val
         green_led.value=green_val
         blue_led.value=blue_val
@@ -46,37 +48,38 @@ def rgb_setter (num_led, red_val, green_val, blue_val):
 # direction returned by function "generate_arrow" from app_function
 def set_arrow(direction):
         while not reset_arrow
-        if (direction == left)
-
-            for num in range(4, 7):  # accende i led per la freccia a sinistra
-                rgb_setter(num, red_val, green_val, blue_val)
-            rgb_setter(0, red_val, green_val, blue_val)
-
-            for num in range(1, 3):  # spegne gli altri led
-                rgb_setter(num, 0, 0, 0)
-
-        elif (direction == right)
-            for num in range(0, 4):
-                rgb_setter(num, red_val, green_val, blue_val)
-
-            for num in range(5, 7)
-                rgb_setter(num, 0, 0, 0)
-
-        elif (direction == front)
-            for num in range(0, 2):
-                rgb_setter(num, red_val, green_val, blue_val)
-            rgb_setter(6, red_val, green_val, blue_val)
-            rgb_setter(7, red_val, green_val, blue_val)
-
-            for num in range(3, 5):  # spegne gli altri led
-                rgb_setter(num, 0, 0, 0)
-
-        elif (direction == behind)
-            for num in range(2, 6):
-                rgb_setter(num, red_val, green_val, blue_val)
-
-            rgb_setter(7, red_val, green_val, blue_val)
-            rgb_setter(0, red_val, green_val, blue_val)
-            rgb_setter(1, red_val, green_val, blue_val)
-
+            if (direction == left)
+    
+                for num in range(4, 7):  # accende i led per la freccia a sinistra
+                    rgb_setter(num, red_val, green_val, blue_val)
+                rgb_setter(0, red_val, green_val, blue_val)
+    
+                for num in range(1, 3):  # spegne gli altri led
+                    rgb_setter(num, 0, 0, 0)
+    
+            elif (direction == right)
+                for num in range(0, 4):
+                    rgb_setter(num, red_val, green_val, blue_val)
+    
+                for num in range(5, 7)
+                    rgb_setter(num, 0, 0, 0)
+    
+            elif (direction == front)
+                for num in range(0, 2):
+                    rgb_setter(num, red_val, green_val, blue_val)
+                rgb_setter(6, red_val, green_val, blue_val)
+                rgb_setter(7, red_val, green_val, blue_val)
+    
+                for num in range(3, 5):  # spegne gli altri led
+                    rgb_setter(num, 0, 0, 0)
+    
+            elif (direction == behind)
+                for num in range(2, 6):
+                    rgb_setter(num, red_val, green_val, blue_val)
+    
+                rgb_setter(7, red_val, green_val, blue_val)
+                rgb_setter(0, red_val, green_val, blue_val)
+                rgb_setter(1, red_val, green_val, blue_val)
+    
+    return 1
 
