2c158b96ee2e0591a9dddcdb73cfa726cc41e6cd
Raffaele Tuzzo <37403051+raffot27@users.noreply.github.com>
2018-06-04 16:58:37 +0200

    smart road sign setting

--- panel/panel_setter.py
+++ panel/panel_setter.py
@@ -1,66 +1,51 @@
-from gpiozero import RGBLED
+from gpiozero import LED, PWMLED
+from time import pause
 from app_functions import generate_arrow , reset_arrow
 
 """
-    RGB positions
-                      0
-                    7   1
-                  6       2
-                    5   3
-                      4
-
+    RGB positions              LED SCANNING CONFIG
+          0                       MATRIX 4X6
+        7   1                    A0 A1 A2 B0 B1 B2
+      6       2      =>       C0 
+        5   3                 C1
+          4                   C2
+                              C3
 """
 
 # definizione LED -> da mettere pin
-leds = [
-    RGBLED(), # 0
-    RGBLED(), # 1
-    RGBLED(), # 2
-    RGBLED(), # 3
-    RGBLED(), # 4
-    RGBLED(), # 5
-    RGBLED(), # 6
-    RGBLED()  # 7
+couple_led = [
+    LED(12), # 0, 4   C0
+    LED(16), # 1, 7   C1
+    LED(20), # 2, 6   C2
+    LED(21) # 3, 5    C3
 ]
 
-def rgb_setter(num_led, red_val, green_val, blue_val):
-    leds[num_led].color = (red_val, green_val, blue_val)
+color_A={
+    "red": PWMLED(11),  #A0 RED
+    "green": PWMLED(5),   #A1 GREEN
+    "blue": PWMLED(6)    #A2 BLUE
+}
 
-# direction returned by function "generate_arrow" from app_function
-def set_arrow(direction, red_val, green_val, blue_val):
-    # right direction
-    if (direction == 0):
-        for num in range(0, 4):
-            rgb_setter(num, red_val, green_val, blue_val)
+color_B={
+    "red": PWMLED(13), #A0 RED
+    "green": PWMLED(19), #A1 GREEN
+    "blue": PWMLED(26)  #A2 BLUE
+}
 
-        for num in range(5, 7):
-            rgb_setter(num, 0, 0, 0)
-    
-    # up direction
-    elif (direction == 1):
-        for num in range(0, 2):
-            rgb_setter(num, red_val, green_val, blue_val)
-        rgb_setter(6, red_val, green_val, blue_val)
-        rgb_setter(7, red_val, green_val, blue_val)
+color[0, 1, 1];
 
-        for num in range(3, 5):  # spegne gli altri led
-            rgb_setter(num, 0, 0, 0)
-    
-    # left direction
-    elif (direction == 2):
-        for num in range(4, 7):  # accende i led per la freccia a sinistra
-            rgb_setter(num, red_val, green_val, blue_val)
-        rgb_setter(0, red_val, green_val, blue_val)
+#if(direction==0)#EST
+        couple_led[0].on()
+        couple_led[1].on()
+        couple_led[2].on()
+        couple_led[3].on()
+        couple_led[4].on()
+        color_B["red"].value(color[0])
+        color_B["green"].value(color[1])
+        color_B["blue"].value(color[2])
 
-        for num in range(1, 3):  # spegne gli altri led
-            rgb_setter(num, 0, 0, 0)
 
-    # down direction
-    elif (direction == 3):
-        for num in range(2, 6):
-            rgb_setter(num, red_val, green_val, blue_val)
 
-        rgb_setter(7, red_val, green_val, blue_val)
-        rgb_setter(0, red_val, green_val, blue_val)
-        rgb_setter(1, red_val, green_val, blue_val)
+
+
 
