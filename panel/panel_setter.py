fc0d9221bdba837a9316010f14695bc6dd96075b
Matteo Corain <m96.corain@gmail.com>
2018-06-05 10:14:06 +0200

    Panel app reorganized

--- panel/panel_setter.py
+++ panel/panel_setter.py
@@ -5,134 +5,84 @@ from time import sleep
     RGB positions              LED SCANNING CONFIG
           0                       MATRIX 3X8
         7   1                 L0 L1 L2 L3 L4 L5 L6 L7
-      6       2      =>      R
-        5   3                G
-          4                  B
+      6       2      =>     R
+        5   3               G
+          4                 B
 """
 
-# definizione LED -> da mettere pin
-leds ={
-    "L0": LED(18), # 0
-    "L1": LED(23), # 1
-    "L2": LED(24), # 2
-    "L3": LED(25), # 3
-    "L4": LED(12), # 4
-    "L5": LED(16), # 5
-    "L6": LED(20), # 6
-    "L7": LED(21)  # 7
-}
+# LEDs definitions
+leds = [
+    LED(18), # 0
+    LED(23), # 1
+    LED(24), # 2
+    LED(25), # 3
+    LED(12), # 4
+    LED(16), # 5
+    LED(20), # 6
+    LED(21)  # 7
+]
 
-color={
+# PWM LEDs definitions
+colors = {
     "red": PWMLED(13),   # REDs
     "green": PWMLED(19), # GREENs
     "blue": PWMLED(26)   # BLUEs
 }
 
+# Correction factors for better color accuracy
+Kcorr = {
+    "red": 0.3,
+    "green": 0.7,
+    "blue": 1
+}
 
-color_set = [0, 0.6, 1]
-direction=0
+def make_arrow(direction: int, red: float, green: float, blue: float):
+    """Make the panel show an arrow pointing to the given direction.
 
-while 1:
-# if(direction==0): #EST
-     leds["L0"].on()
-     leds["L1"].on()
-     leds["L2"].on()
-     leds["L3"].on()
-     leds["L4"].on()
-     leds["L5"].off()
-     leds["L6"].off()
-     leds["L7"].off()
-     color["red"].value = color_set[0]
-     color["green"].value = color_set[1]
-     color["blue"].value = color_set[2]
+    :param direction: The direction the arrow will point
+    :type direction: int
+    :param red: The red value for the LEDs
+    :type red: float
+    :param green: The green value for the LEDs
+    :type green: float
+    :param blue: The blue value for the LEDs
+    :type blue: float
+    """
 
-     sleep(3)
+    # Shut down every LED and set color
+    for led in leds:
+        led.off()
 
+    # Set the colors
+    colors["red"].value = Kcorr["red"] * red
+    colors["green"].value = Kcorr["green"] * green
+    colors["blue"].value = Kcorr["blue"] * blue
 
-    leds["L0"].off()
-    leds["L1"].off()
-    leds["L2"].off()
-    leds["L3"].off()
-    leds["L4"].off()
-    leds["L5"].off()
-    leds["L6"].off()
-    leds["L7"].off()
-    sleep(1)
+    # Light up the neeeded LEDs
+    arrow_leds = []
 
-# if(direction==1):  SUD
-color["red"].value = 0.3
-color["green"].value = 0
-color["blue"].value = 1
+    if direction == 0:
+        # East direction
+        arrow_leds = [0, 1, 2, 3, 4]
+    elif direction == 1:
+        # South direction
+        arrow_leds = [2, 3, 4, 5, 6]
+    elif direction == 2:
+        # West direction
+        arrow_leds = [4, 5, 6, 7, 0]
+    elif direction == 3:
+        # North direction
+        arrow_leds = [6, 7, 0, 1, 2]
+    
+    for i in arrow_leds:
+        leds[i].blink()
 
-     leds["L2"].on()
-     leds["L3"].on()
-     leds["L4"].on()
-     leds["L5"].on()
-     leds["L6"].on()
-     leds["L7"].off()
-     leds["L0"].off()
-     leds["L1"].off()
+    # Sleep to watch LEDs blinking
+    sleep(5)
 
-     sleep(3)
-     direction=3
-     leds["L2"].off()
-     leds["L3"].off()
-     leds["L4"].off()
-     leds["L5"].off()
-     leds["L6"].off()
-     leds["L7"].off()
-     leds["L0"].off()
-     leds["L1"].off()
-     sleep(1)
-# if(direction==2):  OVEST
-     color["red"].value=0.3
-     color["green"].value=0.5
-     color["blue"].value =1
-
-    leds["L4"].on()
-     leds["L5"].on()
-     leds["L6"].on()
-     leds["L7"].on()
-     leds["L0"].on()
-     leds["L1"].off()
-     leds["L2"].off()
-     leds["L3"].off()
-
-     sleep(3)
-
-     leds["L4"].off()
-     leds["L5"].off()
-     leds["L6"].off()
-     leds["L7"].off()
-     leds["L0"].off()
-     leds["L1"].off()
-     leds["L2"].off()
-     leds["L3"].off()
-
-     sleep(1)
-
-#if(direction==3): NORD
-color["red"].value = 0.3
-color["green"].value = 1
-color["blue"].value = 0
-
-leds["L6"].on()
-leds["L7"].on()
-leds["L0"].on()
-leds["L1"].on()
-leds["L2"].on()
-leds["L3"].off()
-leds["L4"].off()
-leds["L5"].off()
-sleep(3)
-
-
-leds["L6"].off()
-leds["L7"].off()
-leds["L0"].off()
-leds["L1"].off()
-leds["L2"].off()
-leds["L3"].off()
-leds["L4"].off()
-leds["L5"].off()
-sleep(1)
+# Test main
+if __name__ == "__main__":
+    color = [0, 0.6, 1]
+    while True:
+        for i in range(4):
+            make_arrow(i, *color)
