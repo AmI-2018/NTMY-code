6ee06e141aff9c81a0dccb8645354bc3cc1d0c34
alessio <ciarcialessio@hotmail.it>
2018-06-12 13:10:53 +0200

    -removed pandas -panel database integrate in the server -bug fix in panel app

--- panel/app.py
+++ panel/app.py
@@ -16,6 +16,8 @@ url = config['serveruri'] + "/login"
 json = {'email': config['email'], 'password': config['rootpwd']}
 session.post(url, json=json)
 
+pl.graph.load_graph(session,config)
+
 # Main Loop
 while True:
     # Search for a nearby user
@@ -23,12 +25,10 @@ while True:
     if user_id == False:
         pi.light_standby()
         continue
-
     # Obtain his/her destination from the server
-    dest_event = af.obtain_dest_by_user(config, user_id, session)
-
+    dest_event = af.obtain_dest_by_user(config, user_id, session)[0]
     # Generate the exit point according to the direction
-    exit_point = pl.route.generate_direction(config['panelID'], dest_event["room"]["nodeID"])
+    exit_point = pl.route.generate_direction(config['panelID'], dest_event["room"]["node"])
 
     # Light up the related arrow
     pi.light_arrow(exit_point, dest_event["color"]["red"], dest_event["color"]["green"], dest_event["color"]["blue"])
--- panel/logic/graph.py
+++ panel/logic/graph.py
@@ -1,10 +1,9 @@
 """ This module initializes the graph """
 import networkx as nx
-import pandas as pd
-import sqlite3
-
 
 edge_weight_name = "distance"
+node_position = {}
+edge_colors = []
 
 """create the graph with networkx"""
 Graph = nx.Graph()
@@ -14,34 +13,33 @@ create a connection with sqlite database "graph.db" where is stored
 all the data concerning the nodes and the edges
 
 """
-if __name__ == "__main__":
-    conn = sqlite3.connect("../database/graph.db")
-else:
-    conn = sqlite3.connect("./database/graph.db")
-
-"""
-store the data in two container, is used pandas in order 
-to handle all this data in a better way
-
-"""
-
-nodelist = pd.read_sql("SELECT * FROM nodes",conn)
-edgelist = pd.read_sql("SELECT * FROM edges",conn)
+edgelist = []
+nodelist = []
 
 
-""" 
-first add the nodes to the graph, each of them with its attributes,
-then add the edges 
+def load_graph(session,config):
+    data = session.get(config["serveruri"]+"/map").json()
+    global edge_colors
+    global node_positions
+    global Graph
+    global edgelist
+    global nodelist
 
-"""
-for i, nlrow in nodelist.iterrows():
-    Graph.add_node(nlrow[0], X=nlrow[1], Y=nlrow[2], orientation=nlrow[3])
+    nodelist = data['nodes']
+    edgelist = data['edges']
 
-for i, elrow in edgelist.iterrows():
-    Graph.add_edge(elrow[0], elrow[1], distance=elrow[2], color=elrow[3])
+    """
+    first add the nodes to the graph, each of them with its attributes,
+    then add the edges 
+    
+    """
 
+    for node in nodelist:
+        Graph.add_node(node['nodeID'], name=node['name'], X=node['X'], Y=node['Y'], orientation=node['orientation'])
 
-""" set the position of each node and the color of each edge """
+    for edge in edgelist:
+        Graph.add_edge(edge['node1']['nodeID'], edge['node2']['nodeID'], distance=edge['distance'], color=edge['color'])
 
-node_positions = {node[0]: (node[1]['X'], node[1]['Y']) for node in Graph.nodes(data=True)}
-edge_colors = [e[2]['color'] for e in Graph.edges(data=True)]
+    # set the position of each node and the color of each edge
+    node_positions = {node[0]: (node[1]['X'], node[1]['Y']) for node in Graph.nodes(data=True)}
+    edge_colors = [e[2]['color'] for e in Graph.edges(data=True)]
